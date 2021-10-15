c0e80404b19ec27b10cc6576316cd57554646060
RL2020 <loc.fitness1@gmail.com>
2021-10-15 15:09:38 -0400

    imported axios api, created a get response from api, rendered response length: images

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import axios from 'axios';
+//import config from '../config'
 import SearchBar from './SearchBar'
+
+//const accessKey = config.access_key
 
 class App extends React.Component {
 
-    onSearchSubmit(term) {
-        console.log(term);
+    state = { images: []};
+
+
+    onSearchSubmit = async (term) => {
+       const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term},
+            headers: {
+                Authorization: 'Client-ID gGDxFofEr0LMgcbkGIkYmOLZE-uoPn8_0oWF9n_t-k4'
+            }
+        });
+        console.log(this);
+        this.setState({ images: response.data.results });
     }
 
     render() {
         return (
             <div className="ui container" style={{margineTop: '10px'}}> 
                 <SearchBar onSubmit={this.onSearchSubmit} /> 
+                Found: {this.state.images.length} images
             </div>
         );
     }
